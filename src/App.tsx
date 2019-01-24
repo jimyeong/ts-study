c2cdef3f68f9a4642557bab2670c73c031d7da25
jimyeong <idjjm92@gmail.com>
2019-01-24 23:20:52 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
 import './App.css';
+import {Store, Unsubscribe} from 'redux';
+import {addAge} from './index';
 
-import logo from './logo.svg';
 
-class App extends React.Component {
-  public render() {
+
+
+
+
+class App extends React.Component <{store:Store<{age:number;}>}>{
+    private _unsubscribe:Unsubscribe;
+
+    componentDidMount() {
+        const store = this.props.store;
+        this._unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        });
+
+
+    }
+
+    componentWillUnmount() {
+        this._unsubscribe();
+    }
+
+
+    public render() {
+        const store = this.props.store;
+        const state = store.getState();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          {state.age}
+          <button onClick={() => {
+              store.dispatch(addAge())
+          }}>
+              한해가 지났다
+          </button>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+
+
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+const ADD_AGE = 'ADD_AGE';
+
+export function addAge(){
+    return {
+        type:ADD_AGE
+    }
+}
+
+function ageApp(state:{age:number;} = {age:35},action:{type:"ADD_AGE"}){
+    if(action.type === ADD_AGE){
+        return{
+            type: state.age+1
+        }
+    }
+    return state
+}
+
+const store = createStore<{age:number;}>(ageApp);
+
+class Provider extends React.Component{
+
+}
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <App store={store}/>,
+    document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+
